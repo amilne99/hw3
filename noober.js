ffd8ad0137f5ee2fb00ffd5d5a2b99817cfe0738
Andrew Milne <Andrew.milne@kellogg.northwestern.edu>
2021-04-17 12:56:22 -0500

    Update/test

--- noober.js
+++ noober.js
@@ -13,5 +13,36 @@ document.addEventListener('DOMContentLoaded', async function() {
   // 🔥 YOUR CODE GOES HERE 🔥
   // Write the recipe (algorithm) in the comments. Then, write the code.
   
+  //Create variables for passenger
+  let passengerFirstName = ride.passengerDetails.first
+  let passengerLastName = ride.passengerDetails.last
+  let passengerPhoneNumber = ride.passengerDetails.phoneNumber
+  let numberOfPassengers = ride.numberOfPassengers
+  
+
+  //Create variables for Pickup
+  let pickupAddress = ride.pickupLocation.address
+  let pickupCity = ride.pickupLocation.city
+  let pickupState =ride.pickupLocation.state
+  let pickupZip =ride.pickupLocation.zip
+
+  //Create variables for Dropoff
+  let dropoffAddress = ride.dropoffLocation.address
+  let dropoffCity = ride.dropoffLocation.city
+  let dropoffState =ride.dropoffLocation.state
+  let dropoffZip =ride.dropoffLocation.zip
+
+  //Create variable for start of expressed based on customer conditional
+  let status
+  if (ride.purpleRequested == true) {
+    status = `Noober Purple`
+  } else if (numberOfPassengers > 3) {
+    status = `Noober XL`
+  } else {
+    status = `Noober X`
+  }
+
+  //Print out the compiled ride order
+  console.log(`${status} Passenger: ${passengerFirstName} ${passengerLastName} - ${passengerPhoneNumber}. Pickup at ${pickupAddress}, ${pickupCity}, ${pickupState} ${pickupZip}. Dropoff at ${dropoffAddress}, ${dropoffCity}, ${dropoffState} ${dropoffZip}`)
   // 🔥 YOUR CODE ENDS HERE 🔥
 })
